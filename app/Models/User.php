b93d22ebb728090a6ba2baf69adae54500c68741
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2022-08-23 08:36:46 +0700

    jwt library
    add jwt library for authentication

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,15 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    protected $table = 'public.tconfuser';
+
+    protected $primaryKey = 'userid';
 
     /**
      * The attributes that are mass assignable.
@@ -41,4 +46,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
