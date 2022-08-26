a430de5048a8c718ad6841ab91bb8feeb9e81aba
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2022-08-26 19:39:01 +0700

    controller
    try to make feature create new data

--- app/Http/Controllers/Api/PersonalInformationManagement.php
+++ app/Http/Controllers/Api/PersonalInformationManagement.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\PersonalInformationManagementRequest;
 use Illuminate\Http\Request;
 
 class PersonalInformationManagement extends Controller
@@ -13,8 +14,12 @@ class PersonalInformationManagement extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function __invoke(PersonalInformationManagementRequest $PIMRequest)
     {
-        //
+        try {
+            
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
     }
 }
